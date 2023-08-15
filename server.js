9fc0d501894277f5e70eb6bdd1238c705a2611a3
Abhinav Tushar <abhinavtata.tushar@gmail.com>
2023-08-15 19:27:52 +0530

    maybe? 💀

--- server.js
+++ server.js
@@ -1,24 +1,58 @@
 const express = require("express");
 const app = express();
-const userRouter = require("./routes/userRoutes");
-const cors=require("cors")
+const cors = require("cors");
 const mongoose = require("mongoose");
-require("dotenv").config()
+require("dotenv").config();
 
-
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
 app.use(express.json());
-// app.use(cors({
-//   origin: "*",
-//   credentials: true,
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//   allowedHeaders: "Content-Type",
-// }));
-app.use(cors({
-  origin: process.env.FRONTEND_URL
-}));
-console.log(`set cors for the frontend at: ${process.env.FRONTEND_URL}`)
+console.log(`set cors for the frontend at: ${process.env.FRONTEND_URL}`);
+const userRouter = require("./routes/userRoutes");
 app.use("/users", userRouter);
-const stripe=require('stripe')(process.env.STRIPE_KEY)
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+app.post("/create-checkout-session", async (req, res) => {
+  const { cart, exchangeRate } = req.body;
+
+  const StoreItems = new Map(
+    cart.items.map((e) => [
+      e.variant_id,
+      {
+        priceInRupees: parseInt(e.unit_price * exchangeRate),
+        name: e.title,
+      },
+    ])
+  );
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: cart.items.map((e) => {
+        const storeItem = StoreItems.get(e.variant_id);
+        return {
+          price_data: {
+            currency: "inr",
+            product_data: {
+              name: storeItem.name,
+            },
+            unit_amount: storeItem.priceInRupees,
+          },
+          quantity: e.quantity,
+        };
+      }),
+      success_url: `${process.env.FRONTEND_URL}/paymentsuccess`,
+      cancel_url: `${process.env.FRONTEND_URL}/paymentfail`,
+    });
+    res.json({ url: session.url });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
 mongoose
   .connect(`${process.env.MONGO_URL}`)
   .then(() => {
@@ -27,38 +61,3 @@ mongoose
     });
   })
   .catch((err) => console.log(err));
-
-  app.post('/create-checkout-session', async (req, res) => {
-    const {  cart, exchangeRate } = req.body;
-    
-    const StoreItems = new Map(cart.items.map((e) => [e.variant_id, {
-      priceInRupees: parseInt(e.unit_price*exchangeRate),
-      name: e.title
-    }]));
-    try{
-      const session=await stripe.checkout.sessions.create({
-        payment_method_types:['card'],
-        mode:'payment',
-        line_items:cart.items.map((e)=>{
-          const storeItem=StoreItems.get(e.variant_id)
-          return{
-            price_data:{
-              currency:'inr',
-              product_data:{
-                name:storeItem.name
-              },
-              unit_amount:(storeItem.priceInRupees)
-            },
-            quantity:e.quantity
-          }
-        }),
-        success_url:`${process.env.FRONTEND_URL}/paymentsuccess`,
-        cancel_url:`${process.env.FRONTEND_URL}/paymentfail`
-      })
-      res.json({url:session.url})
-    }catch(e)
-    {
-      res.status(500).json({error:e.message})
-    }
-  
-  });
