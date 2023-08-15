4bbc26c4aaf317f9f5e91f7fb14409c8d7d09734
Abhinav Tushar <abhinavtata.tushar@gmail.com>
2023-08-15 15:28:48 +0530

    cors change 3

--- server.js
+++ server.js
@@ -9,15 +9,11 @@ require("dotenv").config()
 app.use(express.json());
 app.use(cors({
   origin: process.env.FRONTEND_URL,
-  credentials: true
+  credentials: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type",
 }));
-app.get("/",(req,res)=>{
-  res.setHeader("Access-Control-Allow-Origin", `${process.env.FRONTEND_URL}`);
-	res.setHeader("Access-Control-Allow-Credentials", "true");
-	res.setHeader("Access-Control-Max-Age", "1800");
-	res.setHeader("Access-Control-Allow-Headers", "content-type");
-	res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
-})
+
 app.use("/users", userRouter);
 const stripe=require('stripe')(process.env.STRIPE_KEY)
 mongoose
